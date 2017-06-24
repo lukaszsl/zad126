cf7b2b29b69f1aedb124643f4cc4b754d375923c
Lukasz Sliczner <lukaszsliczner@MacBook-Pro.local>
2017-06-24 22:11:36 +0000

    added makeTable function

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,45 @@
 var url = 'https://restcountries.eu/rest/v1/name/',
-	countriesList = $('#countries');
+	$table = $('country-table');
 
 $('#search').click(searchCountries);
 
 function searchCountries() {
-	var countryName = $('#country-name').val();
+	var countryName = $('#country-name-input').val();
 	if (!countryName.length) countryName = 'Poland';
 
 	$.ajax({
 		url: url + countryName,
 		method: 'GET',
-		success: showCountriesList
+		success: makeTable
 	});
 }
 
+function makeTable(resp) {
+
+	resp.forEach(function(item) {
+		var $country = $('<div>').addClass('country-table'),
+			$countryNameHeader = $('<h2>').addClass('country-name-th'),
+			$countryTable = $('<table>'),
+			$tableRow = $('<tr>'),
+			$nameColumn = $('<td>'),
+			$valueColumn = $('<td>');
+
+		$country.appendTo('#body');
+		$countryNameHeader.text(item.name).appendTo($country);
+		$countryTable.appendTo($country);
+		$tableRow.appendTo($countryTable);
+		$nameColumn.text('capital: ').appendTo($tableRow);
+		$valueColumn.text(item.capital).appendTo($tableRow);
+	});
+
+
+}
+
+/*
 function showCountriesList(resp) {
 	countriesList.empty();
 	resp.forEach(function(item) {
 		$('<li>').text(item.name).appendTo(countriesList);
 	});
 }
+*/
